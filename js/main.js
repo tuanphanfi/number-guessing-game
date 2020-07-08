a8750915a9cc957bff07009448e559db4210ddd5
tuanphanfi <tuanphan.fi@gmail.com>
2020-07-08 15:50:39 +0700

    mini rocket

--- js/main.js
+++ js/main.js
@@ -3,21 +3,29 @@ let selectHistory = document.getElementById('history')
 //generate randomNumber
 let randomNumber = Math.round(Math.random() * 10);
 //remaining guess count
-let remainingGuessCount = 3;
+let remainingGuessCount = 10;
 //reamining guess select
 let selectRemainingGuess = document.getElementById('remaining-guess')
+// select notification
+let notification = document.getElementById('notification')
+//history array implemation
+let history = []
+//selectRemainingGuess.innerHTML = remainingGuessCount;
+selectRemainingGuess.innerHTML = remainingGuessCount;
 function insertToHistory(value) {
-    selectHistory.innerHTML = value;
+    notification.innerHTML = value;
 }
 
 let feedBack = [`Yay, congrastulation. You make the right guess`, `Your guess is higher`, `Your guess is lower`]
 
+let i = 0;
 guess = () => {
-    selectRemainingGuess.innerHTML = remainingGuessCount;
-    console.log(randomNumber)
-    let yourGuess = parseInt(selectYourGuess.value);
-    if (remainingGuessCount > 0) {
+    timecounting()// fire the timecounting function!!
 
+    let yourGuess = parseInt(selectYourGuess.value);
+
+    if (remainingGuessCount > 0) {
+        remainingGuessCount--;
         // console.log(remainingGuessCount)
         if ((yourGuess) === randomNumber) {
             insertToHistory(`${feedBack[0]} The number is ${randomNumber}`)
@@ -26,18 +34,55 @@ guess = () => {
         } else if (yourGuess < randomNumber) {
             insertToHistory(`Your guess is lower`)
         }
-        remainingGuessCount--;
+
     } else {
         insertToHistory(`You are out of guess!!!`)
     }
+    selectRemainingGuess.innerHTML = remainingGuessCount;
+    console.log("Random Number", randomNumber)
 
+    //push into array
+    if (i < 3) {
+        history.push(yourGuess)
+        selectHistory.innerHTML += history[i] + ', '
+        i++;
+    } else {
+        history[i]
+    }
 
+    // for (let i = 0; i < history.length; i++) {
+    //     selectHistory.innerHTML += history[i] + ', '
+    //     // if(i 2 )
+
+    // }
 }
 
 reset = () => {
+    remainingGuessCount = 3;
+
+    selectHistory.innerHTML = "";
+    history.splice(0, 3)
     selectRemainingGuess.innerHTML = remainingGuessCount;
-    remainingGuess = 3;
+
     insertToHistory(`Let's play a new game`)
     randomNumber = Math.round(Math.random() * 10);
-
+    // timecounting()// fire the timecounting function!!
+    time = 5;
 }
+
+
+let time = 5    // time start from 0
+let myTime; // timer will be assign to this variable
+function timecounting() {
+    myTime = setInterval(() => {
+        if (time > 0) {
+            time--
+            document.getElementById('timecount').innerHTML = time
+        } else {
+            document.getElementById('timecount').innerHTML = "Running out of time :(."
+        }
+
+
+    }, 1000)// every 1 second, it will add 1 into time variable (computer use millisecond so 1000 is 1 second)
+}
+timecounting()// fire the timecounting function!!
